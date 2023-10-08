f6470fbcaeadc479bde5b98bcbd784379cdbf577
GeneralMari0 <93846028+GeneralMari0@users.noreply.github.com>
2023-10-07 17:43:43 -0700

    removed unused files

--- app/src/main/java/ca/blisstudios/shopsmart/ProductEvent.kt
+++
@@ -1,6 +0,0 @@
-package ca.blisstudios.shopsmart
-
-sealed interface ProductEvent{
-    object clickPrice: ProductEvent
-    data class readPrice(val price: Float): ProductEvent
-}
--- app/src/main/java/ca/blisstudios/shopsmart/Screen.kt
+++
@@ -1,6 +0,0 @@
-package ca.blisstudios.shopsmart
-
-sealed class Screen(val route: String){
-    object HomeScreen : Screen("home_screen")
-    object ProductScreen : Screen("product_screen")
-}
